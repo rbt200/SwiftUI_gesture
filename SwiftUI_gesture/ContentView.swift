6d18afdb4def7e926efc48c2dc2896f58203a40b
rbt200 <myesoap@mail.ru>
2020-05-03 10:10:41 -0700

    Add rotation effect

--- SwiftUI_gesture/ContentView.swift
+++ SwiftUI_gesture/ContentView.swift
@@ -12,11 +12,19 @@ struct ContentView: View {
     
     @State private var isTapped: Bool = false
     @State private var cardDragState = CGSize.zero
+    @State private var cardRotateState: Double = 0
     
     var body: some View {
         Card(tapped: self.isTapped)
             .animation(.spring())
             .offset(y: self.cardDragState.height)
+            .rotationEffect(Angle(degrees: self.cardRotateState))
+            
+        .gesture(RotationGesture()
+            .onChanged { value in
+                self.cardRotateState = value.degrees
+        }        )
+            
             .gesture(DragGesture()
                 .onChanged { value in
                     self.cardDragState = value.translation
