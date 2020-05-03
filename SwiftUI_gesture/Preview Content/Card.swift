9393109b4d0f538062324d7fda1a41efecf91a24
rbt200 <myesoap@mail.ru>
2020-05-03 10:03:02 -0700

    Add magnification

--- SwiftUI_gesture/Preview Content/Card.swift
+++ SwiftUI_gesture/Preview Content/Card.swift
@@ -11,17 +11,28 @@ import SwiftUI
 struct Card: View {
     
     let tapped: Bool
+    @State private var scale: CGFloat = 1.0
     
     var body: some View {
         
         VStack {
+            
+            Image("child")
+                .resizable()
+                .scaleEffect(self.scale)
+                .frame(width: 300, height: 300)
+                .gesture(MagnificationGesture()
+                    .onChanged { value in
+                        self.scale = value.magnitude
+                    }
+            )
             Text("Card")
                 .font(.largeTitle)
                 .foregroundColor(Color.white)
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
         .background(tapped ? Color.purple : Color.red)
-    .cornerRadius(30)
+        .cornerRadius(30)
         
     }
 }
