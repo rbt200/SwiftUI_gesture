4eba9a06eb00074f6301a7c238e04b01c4432985
rbt200 <myesoap@mail.ru>
2020-05-03 09:25:53 -0700

    Add drag gesture and animation

--- SwiftUI_gesture/ContentView.swift
+++ SwiftUI_gesture/ContentView.swift
@@ -11,13 +11,25 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var isTapped: Bool = false
+    @State private var cardDragState = CGSize.zero
     
     var body: some View {
         Card(tapped: self.isTapped)
-        .gesture(TapGesture(count: 1)
-            .onEnded({ () in
-                self.isTapped.toggle()
-            }))
+            .animation(.spring())
+            .offset(y: self.cardDragState.height)
+            .gesture(DragGesture()
+                .onChanged { value in
+                    self.cardDragState = value.translation
+                }
+            .onEnded { value in
+                self.cardDragState = CGSize.zero
+                }
+        )
+            
+            .gesture(TapGesture(count: 1)
+                .onEnded({ () in
+                    self.isTapped.toggle()
+                }))
     }
 }
 
